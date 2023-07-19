480500c7609e2a6bc3b626cbdbed2b3d640fc73a
mocha-matcha <sana.ali.said.mohammed@gmail.com>
2023-07-19 10:45:38 -0500

    added top5 functionality.

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,3 @@
-//new Glide('.glide').mount()
-
 var searches = [];
 let appId = "54521986645452198664";
 const cleanSearch = (searchTerm) => {
@@ -10,91 +8,6 @@ const cleanSearch = (searchTerm) => {
 const clearHistory = () => {
   $("#search-results").empty();
 };
-
-//Working on api,
-// atm has a 2nd parameter to check search type later this type variable will be decided by the val of the dropdown.
-// const search = async (searchTerm) => {
-//   clearSearch();
-//   console.log($("#dropdown").val());
-
-//   // const st = cleanSearch(searchTerm);
-//   const url = `https://deezerdevs-deezer.p.rapidapi.com/search?q=${searchTerm}`;
-
-//   console.log(url);
-
-//   const options = {
-//     method: "GET",
-//     headers: {
-//       "X-RapidAPI-Key": "444d2ddda8mshf9bee96999e82f2p1c6386jsn9d2170e8e31d",
-//       "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
-//     },
-//   };
-
-//   const response = await fetch(url, options);
-//   console.log(response);
-//   const result = await response.json();
-//   console.log(result);
-
-//   let searchs = result["data"];
-//   console.log(searchs);
-
-//   let cardData = [];
-//   let formatValue = $("#dropdown").val();
-
-//   console.log(formatValue);
-//   if (formatValue === "artist") {
-//     let artistId = searchs[0]["artist"]["id"];
-//     console.log(artistId);
-//     // result.filter();
-
-//     let artistMatches = searchs.filter((data) => {
-//       return data["artist"]["id"] === artistId;
-//     });
-
-//     console.log(artistMatches);
-//     artistMatches.forEach(async (element) => {
-//       $("#search-results").append(
-//         await getCardElement(
-//           element["title"],
-//           element["title_short"],
-//           element["artist"]["picture"],
-//           element["link"]
-//         )
-//       );
-//     });
-//   } else if (formatValue === "album") {
-//     let albumId = searchs[0]["album"]["id"];
-//     console.log(albumId);
-//     let albumMatches = searchs.filter((data) => {
-//       return data["album"]["id"] === albumId;
-//     });
-//     console.log(albumMatches);
-//     albumMatches.forEach(async (element) => {
-//       $("#search-results").append(
-//         await getCardElement(
-//           element["title"],
-//           element["title_short"],
-//           element["album"]["cover"],
-//           element["link"]
-//         )
-//       );
-//     });
-//   } else {
-//     searchs.forEach(async (element) => {
-//       $("#search-results").append(
-//         await getCardElement(
-//           element["title"],
-//           element["title_short"],
-//           element["album"]["cover"],
-//           element["link"]
-//         )
-//       );
-//     });
-//   }
-
-//   let newUrl = `searchResult.html?q=${cleanSearch(searchTerm)}&f=${formatValue}`;
-//   location.assign(newUrl);
-// };
 
 // priotty of dropdown value
 const getCardElement = async (name, smallName, image, link) => {
@@ -171,4 +84,6 @@ const searchHistory = () => {
   });
 };
 searchHistory();
+
 $("#search-button").on("click", startSearch);
+
